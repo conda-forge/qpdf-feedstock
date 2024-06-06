fda7bc2f7091673150b2f7c9b00d637148d726ac
jakirkham <jakirkham@users.noreply.github.com>
2024-06-06 00:47:49 -0700

    Add initial Windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,19 @@
+set CMAKE_CONFIG=Release
+
+mkdir build_%CMAKE_CONFIG%
+if %ERRORLEVEL% neq 0 exit 1
+
+pushd build_%CMAKE_CONFIG%
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG% ^
+    -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DBUILD_STATIC_LIBS:BOOL=OFF ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"
+if %ERRORLEVEL% neq 0 exit 1 
+
+cmake --build . --target install --config %CMAKE_CONFIG%
+if %ERRORLEVEL% neq 0 exit 1
+
+popd
