aa26e0fa09a48a5ae1173a30965ca314ddd530a7
jakirkham <jakirkham@users.noreply.github.com>
2024-06-06 02:28:43 -0700

    Update build to use CMake

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
 
-./autogen.sh
-./configure --prefix="${PREFIX}"
-make install
+mkdir build_release
+pushd build_release
+
+cmake ${CMAKE_ARGS} \
+    -GNinja \
+    -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+    -DINSTALL_HEADERS:BOOL=ON \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DBUILD_STATIC_LIBS:BOOL=OFF \
+    -DBUILD_TESTING:BOOL=ON \
+    -DREQUIRE_CRYPTO_OPENSSL:BOOL=ON \
+    "${SRC_DIR}"
+
+
+popd  # Leave `build_release`
